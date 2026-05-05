a57147497127398ba3dba7c5e15ce5e07344002f
Utkarshs9924 <utkarshs1199@gmail.com>
2026-05-04 19:35:13 -0700

    feat: AWS EKS deployment - fix Kafka broker, topics, analytics worker

--- client/src/api/connectionApi.js
+++ client/src/api/connectionApi.js
@@ -1,4 +1,4 @@
-const CONNECTION_API = "http://a646a4c5e82b9472997248fb7c128493-a29f34db7d83c969.elb.us-east-2.amazonaws.com:8007";
+const CONNECTION_API = "http://a35d09762551d4c84a449e5e8372ef9c-cfb918ea0cf7f007.elb.us-east-2.amazonaws.com:8007";
 
 function extractErrorMessage(data, status) {
   if (!data) return `Request failed: ${status}`;
